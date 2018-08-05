81c3b0401c183e58f6c5f30b1509064f271040db
aboud20-meet <aboud20@meet.mit.edu>
2018-08-05 13:59:16 +0300

    classes

--- exercises/superheros.py
+++ exercises/superheros.py
@@ -1,1 +1,17 @@
 # Write your solutions for 1.5 here!
+
+class superheros:
+    def __init__(self,name,superpower,strength):
+        self.name=name
+        self.superpower=superpower
+        self.strength=strength
+
+superhero1= superheros("batman", "fly", 10)
+superhero2= superheros("superman", "run",8)
+
+#print(superhero1.name)
+#print(superhero1.strength)
+
+
+def save_civilian(work):
+    
--- exercises/while_loops.py
+++ exercises/while_loops.py
@@ -1,1 +1,20 @@
 # Write your solution for 1.3 here!
+
+#ALL NUMBERS
+# n=0
+# sum=0
+# while sum<=10000:
+#     n=n+1
+#     sum=sum+n
+# print(sum)
+# print(n)
+
+
+#ONLY EVEN NUMBERS
+n=0
+sum=0
+while sum<=10000:
+    n=n+2
+    sum=sum+n
+print(sum)
+print(n)
