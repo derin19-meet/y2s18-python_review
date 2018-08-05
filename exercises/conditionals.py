528bf2fdc1262775478b4ede5e8494f65f32c64e
aboud20-meet <aboud20@meet.mit.edu>
2018-08-05 11:57:43 +0300

    python

--- exercises/conditionals.py
+++ exercises/conditionals.py
@@ -1,1 +1,5 @@
 # Write your solution for 1.2 here!
+sum=0
+for i in range(0,101,2):
+    sum=sum+i
+print(sum)
--- exercises/for_loops.py
+++ exercises/for_loops.py
@@ -1,1 +1,6 @@
 # Write your solution for 1.1 here!
+n=101
+sum=0
+for i in range(1,n):
+    sum=sum+i
+print(sum)
